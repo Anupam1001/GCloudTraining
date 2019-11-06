51d8b59fb4294b7248b9da703a463e9bd5014f1e
Anupam KUmar <Anupam-kumar@hotmail.com>
2019-11-06 16:03:04 +0530

    Update creating_sample_vm.sh

--- creating_sample_vm.sh
+++ creating_sample_vm.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 gcloud beta compute --project=euphoric-diode-254304 instances create instance-1 --zone=us-central1-a --machine-type=n1-standard-1
  --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=449982624529-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/de
 vstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapi
